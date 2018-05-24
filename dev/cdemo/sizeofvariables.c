1ac3c2f9701f93a7576f553904339ad82c42f183
finkelmanje <seanliu0425@gmail.com>
2018-05-24 15:58:39 +0000

    Second commit of sizeofvariables

--- dev/cdemo/sizeofvariables.c
+++ dev/cdemo/sizeofvariables.c
@@ -5,16 +5,16 @@ int main()
   int a = 545;
   float b = 2.3;
   double c =500.324;
-  char d = a;
+  char d = 'A';
   unsigned int e = 25;
   short int f = 314;
   // print value and size of an int variable
-  printf("int a value %d (oct %o, hex %x) and size %d bytes.\n", a, a, a, sizeof(a));
-  printf("float b value %d (oct %o, hex %x) and size %d bytes.\n", b, b, b, sizeof(b));
-  printf("double c value %d (oct %o, hex %x) and size %d bytes.\n", c, c, c, sizeof(c));
-  printf("char d value %d (oct %o, hex %x) and size %d bytes.\n", d, d, d, sizeof(d));
-  printf("unsigned int e value %d (oct %o, hex %x) and size %d bytes.\n", e, e, e, sizeof(e));
-  printf("short int f value %d (oct %o, hex %x) and size %d bytes.\n", f, f, f, sizeof(f));
+  printf("int a value %d has size %d bytes.\n", a, sizeof(a));
+  printf("float b value %f has size %d bytes.\n", b, sizeof(b));
+  printf("double c value %f has size %d bytes.\n", c, sizeof(c));
+  printf("char d value %c has size %d bytes.\n", d, sizeof(d));
+  printf("unsigned int e value %d has size %d bytes.\n", e, sizeof(e));
+  printf("short int f value %d has  size %d bytes.\n", f, sizeof(f));
 
 }
 
